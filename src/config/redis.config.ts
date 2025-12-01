80322bdf2b68b260fce8737c2f8afeb755bbc196
harvey ana <harveyana3@gmail.com>
2025-12-01 11:27:23 +0100

    fixes for instance

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -8,22 +8,14 @@ import config from '../config/env.config';
 
 
 const connection = new IORedis({
-    host: config.redisHost,
+    host: config.redisHost || 'localhost',
     port: parseInt(config.redisPort),
     username: "default",
-    password: config.redisPassWord,
+    // password: config.redisPassWord,
     maxRetriesPerRequest: null,
     enableReadyCheck: false,
-    // lazyConnect: true, // Don't connect immediately
-     // Connection pooling settings
-     // connectTimeout: 30000,
-     // commandTimeout: 30000,
-     // Limit concurrent connections
-    // maxLoadingRetryTime: 10000,
-    // retryDelayOnFailover: 1000,
-    // connectTimeout: 45000,
-    // commandTimeout: 45000,
-    // retryDelayOnFailover: 2000
+    lazyConnect: true, // Don't connect immediately
+     
 });
 
 // Handle connection errors
