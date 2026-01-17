9f39160f957a20a0c5ca74b9c70c17841aa99926
harvey ana <harveyana3@gmail.com>
2026-01-17 19:08:48 +0100

    added clerk auth middleware

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,6 +3,7 @@ import { UserInfoClient } from 'auth0';
 import { NextFunction, Request, Response } from 'express';
 import { verifyAccessToken } from '../utils';
 import prisma from '../config/prisma.config';
+import { clerkClient, requireAuth, getAuth } from '@clerk/express'
 
 dotenv.config();
 
@@ -13,35 +14,35 @@ export const authMiddleware = async (
   next: NextFunction
 ) => {
   // 1. Extract Token
-  const { authorization } = req.headers;
-  if (!authorization) {
-    return res.status(401).json({ 
-      success: false,
-      error: 'Missing authorization token' 
-    });
-  }
+  // const { authorization } = req.headers;
+  // if (!authorization) {
+  //   return res.status(401).json({ 
+  //     success: false,
+  //     error: 'Missing authorization token' 
+  //   });
+  // }
 
-  const token = authorization.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      error: 'Malformed authorization header'
-    });
-  }
+  // const token = authorization.split(' ')[1];
+  // if (!token) {
+  //   return res.status(401).json({
+  //     success: false,
+  //     error: 'Malformed authorization header'
+  //   });
+  // }
 
   try {
     // 2. Validate Token (choose ONE approach)
     
     // OPTION A: Local verification (recommended)
-    const { success, data } = verifyAccessToken(token);
-    if (!success || !data?.userId) {
-      console.log('token data',data)
+    // const { success, data } = verifyAccessToken(token);
+    // if (!success || !data?.userId) {
+    //   console.log('token data',data)
         
-      return res.status(403).json({
-        success: false,
-        error: 'Invalid or expired token'
-      });
-    }
+    //   return res.status(403).json({
+    //     success: false,
+    //     error: 'Invalid or expired token'
+    //   });
+    // }
 
     // OPTION B: Remote verification (if you need fresh claims)
     // const userInfo = await userInfoClient.getUserInfo(token);
@@ -50,9 +51,12 @@ export const authMiddleware = async (
     // }
     // const authId = userInfo.data.sub;
 
+    const { userId } = getAuth(req)
+    const data = await clerkClient.users.getUser(userId as string)
+
     // 3. Find/Create User
     let user = await prisma.user.findUnique({
-      where: { id: data?.userId },
+      where: { id: userId as string },
       select: {
         id: true,
         firstName: true,
@@ -65,12 +69,12 @@ export const authMiddleware = async (
     if (!user) {
       user = await prisma.user.create({
         data: {
-          id: data.userId,
-          email: data.email as string,
+          id: data.id,
+          email: data.primaryEmailAddress?.emailAddress as string,
           firstName: data.firstName || '',
           lastName: data.lastName || '',
-          emailVerified: data.emailVerified || false,
-          photoUrl: data.photoUrl
+          emailVerified: data.primaryEmailAddress?.verification?.status === 'verified' || false,
+          photoUrl: data.imageUrl
         },
         select: {
           id: true,
