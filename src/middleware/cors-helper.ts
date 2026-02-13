3484df889056ad4a6a4dd1a3e0b1097cc44d5bb5
harvey ana <harveyana3@gmail.com>
2026-02-13 02:40:05 +0100

    still preflight fixes

--- src/middleware/cors-helper.ts
+++ src/middleware/cors-helper.ts
@@ -7,10 +7,10 @@ import { Request, Response, NextFunction } from 'express';
 
 export const requireAuthWithCORS = () => {
   return (req: Request, res: Response, next: NextFunction) => {
-    // // Skip OPTIONS
-    // if (req.method === 'OPTIONS') {
-    //   return next();
-    // }
+    // Skip OPTIONS
+    if (req.method === 'OPTIONS') {
+      return next();
+    }
     
     // Apply Clerk auth
     return requireAuth()(req, res, next);
