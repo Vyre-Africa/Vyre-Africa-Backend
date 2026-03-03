9731c280867c57953f9b52ccd4e0470ccaa23ff2
harvey ana <harveyana3@gmail.com>
2026-03-03 22:58:21 +0100

    moved up cors

--- src/app.ts
+++ src/app.ts
@@ -14,31 +14,6 @@ import { router } from './routes';
 
 dotenv.config();
 
-// FOR WEBHOOK handler
-app.use('/api/v1/webhook/fern', express.raw({ type: 'application/json', limit: '10mb' }));
-app.use('/api/v1/webhook/clerk', express.raw({ type: 'application/json', limit: '10mb' }));
-// app.use('/api/v1/tatum/events', express.raw({ type: 'application/json', limit: '10mb' }));
-// app.use('/api/v1/webhook', express.raw({ type: 'application/json', limit: '10mb' }));
-
-app.use(express.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (req, res) => res.send('Vyre Backend!'));
-
-// initializeAdmin();
-
-app.use(compression());
-app.use(morgan('dev'));
-app.use(cookieParser());
-
-// app.use(function (req, res, next) {
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept',
-//     );
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// });
-
 const corsOptions = {
     origin: [
       'http://localhost:3000',
@@ -65,6 +40,33 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+// FOR WEBHOOK handler
+app.use('/api/v1/webhook/fern', express.raw({ type: 'application/json', limit: '10mb' }));
+app.use('/api/v1/webhook/clerk', express.raw({ type: 'application/json', limit: '10mb' }));
+// app.use('/api/v1/tatum/events', express.raw({ type: 'application/json', limit: '10mb' }));
+// app.use('/api/v1/webhook', express.raw({ type: 'application/json', limit: '10mb' }));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.send('Vyre Backend!'));
+
+// initializeAdmin();
+
+app.use(compression());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+// app.use(function (req, res, next) {
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept',
+//     );
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
+
+
 
 app.use('/api/v1', router);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
